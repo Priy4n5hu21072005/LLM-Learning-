0ad3516b45632bdb5b455f125c3b34c701c405f9
Priy4n5hu21072005 <priyanshukumar08357@gmail.com>
2026-09-18 18:44:36 +0530

    fix the issue

--- Agentic Ai/New Start of Agentic AI/Email_Agent/API/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-app=FastAPI()
-@app.get("/")
-def message_function():
-    return {"message":"Hi the fastapi backend is ready to devlop"}
--- Agentic Ai/New Start of Agentic AI/Email_Agent/API/schema.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-class EmailResponse(BaseModel):
-    subject:str
-    body:str
